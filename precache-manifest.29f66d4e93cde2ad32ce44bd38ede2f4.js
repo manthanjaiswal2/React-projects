a446e1c59f7ab2d3b15fdac71450484fa1ba6aef
manthanjaiswal2 <35054059+manthanjaiswal2@users.noreply.github.com>
2020-01-31 19:29:21 +0530

    Updates

---
+++ precache-manifest.29f66d4e93cde2ad32ce44bd38ede2f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18eb5868ae911b905ad2bb430b23a5b5",
+    "url": "/React-projects/index.html"
+  },
+  {
+    "revision": "1bb9cc1047061dcc32b7",
+    "url": "/React-projects/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5be9c45f41e7b1a91fe8",
+    "url": "/React-projects/static/js/2.0f7a72b1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-projects/static/js/2.0f7a72b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bb9cc1047061dcc32b7",
+    "url": "/React-projects/static/js/main.8b5575f0.chunk.js"
+  },
+  {
+    "revision": "e53a7f455b5c09f29474",
+    "url": "/React-projects/static/js/runtime-main.c128bd90.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-projects/static/media/logo.5d5d9eef.svg"
+  }
+]);
